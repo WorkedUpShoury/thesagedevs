34b7a2dfd2056fd96a67721df638aa0a8f783ccd
Shoury Sinha <Contactshoury@gmail.com>
2025-07-17 17:21:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -111,23 +111,5 @@ function openWindow(url) {
 
 function hireMe() {
   const gmailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=${myEmail}&su=${subject}&body=${body}`;
-  const outlookURL = `https://outlook.office.com/mail/deeplink/compose?to=${myEmail}&subject=${subject}&body=${body}`;
-  const yahooURL = `https://compose.mail.yahoo.com/?to=${myEmail}&subject=${subject}&body=${body}`;
-  const mailtoURL = `mailto:${myEmail}?subject=${subject}&body=${body}`;
-
-  // Try Gmail
-  if (openWindow(gmailURL)) return;
-
-  // Try Outlook
-  setTimeout(() => {
-    if (openWindow(outlookURL)) return;
-
-    // Try Yahoo
-    setTimeout(() => {
-      if (openWindow(yahooURL)) return;
-
-      // Fallback to mailto
-      window.location.href = mailtoURL;
-    }, 800);
-  }, 800);
+  openWindow(gmailURL);
 }
